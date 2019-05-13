2aa9e46d9c1ac11929c8e1bcc3057e8ae351873d
William Chou <williamchoudev@gmail.com>
2019-05-13 01:42:03 -0400

    Replace stylesheet with styled-components GlobalStyle

--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,45 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
 
-import './index.scss';
-import App from './components/TodoApp';
+// Styles
+import 'antd/dist/antd.css'
+import 'normalize.css';
+import { createGlobalStyle } from 'styled-components';
+
+
+import TodoApp from './components/TodoApp';
 import * as serviceWorker from './serviceWorker';
+
+const GlobalStyle = createGlobalStyle`
+body {
+  margin: 0;
+  padding: 0;
+  font-family: "-apple-system",
+    "BlinkMacSystemFont",
+    "Segoe UI", "Roboto", "Oxygen",
+    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  background-image: linear-gradient(to right, #a8edea 0%, #fed6e3 100%);
+}
+
+code {
+  font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New",
+    monospace;
+}
+
+`
 
 const store = configureStore();
 
+
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <>
+    <GlobalStyle />
+    <Provider store={store}>
+      <TodoApp />
+    </Provider></>,
   document.getElementById('root')
 );
 
